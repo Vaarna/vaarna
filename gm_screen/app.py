4968f6f4bb43398ff76234331d9a8046ebb7c582
Maximilian Remming <maximilian@remming.fi>
2020-11-07 22:52:55 +0200

    Add CORS support and remove old test APIs

--- gm_screen/app.py
+++ gm_screen/app.py
@@ -3,37 +3,19 @@ import hashlib
 import typing as t
 
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 
 from . import assets
+from .config import get_settings
 
 app = FastAPI()
 app.include_router(assets.router, prefix="/assets")
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: t.Optional[bool] = None
-
-
-@app.get("/")
-def read_root():
-    resp = """
-<!DOCTYPE html>
-<html>
-<video src="http://localhost:8000/assets/-KFsLKDKhtxiuZ24TeWgZyTApfKE1H7RyuR_9HyRqgA=" controls>
-"""
-
-    return HTMLResponse(resp)
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: t.Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=get_settings().cors_allow_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
--- gm_screen/config.py
+++ gm_screen/config.py
@@ -1,3 +1,4 @@
+import typing as t
 from functools import lru_cache
 
 from pydantic import BaseSettings
@@ -6,6 +7,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     asset_bucket: str
     asset_key_prefix: str = ""
+    cors_allow_origins: t.List[str]
 
 
 @lru_cache()
