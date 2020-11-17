f6d931a1625a3fe723d988f9038863e3b0093afb
Maximilian Remming <maximilian@remming.fi>
2020-11-17 20:39:26 +0200

    Improve the tests

--- frontend/src/utils.test.ts
+++ frontend/src/utils.test.ts
@@ -1,24 +1,27 @@
 import { round, randomId } from "./utils";
 
-test("randomId creates a different id on calls", () => {
-  const a = randomId(),
-    b = randomId();
+describe("randomId", () => {
+  test("creates a different id on calls", () => {
+    expect(randomId()).not.toEqual(randomId());
+  });
 
-  expect(a).not.toEqual(b);
+  test("is at least 12 characters long", () => {
+    expect(randomId().length).toBeGreaterThanOrEqual(12);
+  });
 });
 
-[
-  [11.11, 0, 11],
-  [11.11, 1, 11.1],
-  [11.11, -1, 10],
+describe("round", () => {
+  test.each([
+    [11.11, 0, 11],
+    [11.11, 1, 11.1],
+    [11.11, -1, 10],
 
-  [111.111, 0, 111],
-  [111.111, 1, 111.1],
-  [111.111, 2, 111.11],
-  [111.111, -1, 110],
-  [111.111, -2, 100],
-].forEach(([v, acc, res]) => {
-  test(`rounding ${v} with accuracy ${acc} should equal ${res}`, () => {
+    [111.111, 0, 111],
+    [111.111, 1, 111.1],
+    [111.111, 2, 111.11],
+    [111.111, -1, 110],
+    [111.111, -2, 100],
+  ])("%p with accuracy %p equals %p", (v, acc, res) => {
     expect(round(v, acc)).toEqual(res);
   });
 });
