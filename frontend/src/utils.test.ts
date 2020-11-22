37419c04915a929c94da19723a8ca53fa317ae1a
Maximilian Remming <maximilian@remming.fi>
2020-11-22 18:13:20 +0200

    Add retry function and tests for it

--- frontend/src/utils.test.ts
+++ frontend/src/utils.test.ts
@@ -1,8 +1,8 @@
-import { randomId, round } from "./utils";
+import { delay, wait, randomId, round, retry } from "./utils";
 
 describe("randomId", () => {
   test("creates a different id on calls", () => {
-    expect(randomId()).not.toEqual(randomId());
+    expect(randomId()).not.toBe(randomId());
   });
 
   test("is at least 12 characters long", () => {
@@ -19,9 +19,113 @@ describe("round", () => {
     [111.111, 0, 111],
     [111.111, 1, 111.1],
     [111.111, 2, 111.11],
+    [111.111, 3, 111.111],
+    [111.111, 4, 111.111],
     [111.111, -1, 110],
     [111.111, -2, 100],
+    [111.111, -3, 0],
   ])("%p with accuracy %p equals %p", (v, acc, res) => {
-    expect(round(v, acc)).toEqual(res);
+    expect(round(v, acc)).toBe(res);
+  });
+});
+
+describe("wait", () => {
+  beforeEach(() => {
+    jest.useRealTimers();
+  });
+
+  test("to resolve with given value", () => {
+    expect.assertions(1);
+
+    return wait(10, "ok").then((v) => {
+      expect(v).toBe("ok");
+    });
+  });
+});
+
+describe("delay", () => {
+  beforeEach(() => {
+    jest.useRealTimers();
+  });
+
+  test("can be used in a promise chain", () => {
+    expect.assertions(1);
+
+    return Promise.resolve("ok")
+      .then(delay(10))
+      .then((v) => {
+        expect(v).toBe("ok");
+      });
+  });
+});
+
+describe("retry", () => {
+  beforeEach(() => {
+    jest.useRealTimers();
+    jest.setTimeout(100);
+  });
+
+  describe("with type of constant", () => {
+    test("resolves to the value of a promise that always resolves", () => {
+      expect.assertions(1);
+
+      return retry(() => Promise.resolve("ok"), {
+        type: "constant",
+        interval: 10,
+      }).then((v) => {
+        expect(v).toBe("ok");
+      });
+    });
+
+    test("rejects to the value of a promise that always rejects", () => {
+      expect.assertions(1);
+
+      return retry(() => Promise.reject("fail"), {
+        type: "constant",
+        interval: 10,
+      }).catch((v) => {
+        expect(v).toBe("fail");
+      });
+    });
+
+    test("resolves immediately if the function resolves on first try", () => {
+      jest.useFakeTimers();
+      expect.assertions(1);
+
+      return retry(() => Promise.resolve("ok"), {
+        type: "constant",
+        interval: 10,
+      }).then(() => {
+        expect(setTimeout).toHaveBeenCalledTimes(0);
+      });
+    });
+
+    test("resolving function is only called once", async () => {
+      expect.assertions(1);
+
+      const fn = jest.fn(() => Promise.resolve("ok"));
+
+      return retry(fn, { type: "constant", interval: 10 }).then((_) => {
+        expect(fn).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    test("rejecting function is retried `maxRetries` times", () => {
+      expect.assertions(7);
+
+      const fn = jest.fn(() => Promise.reject("fail"));
+
+      return retry(fn, { type: "constant", interval: 10, maxRetries: 5 }).catch(
+        (_) => {
+          expect(fn).toHaveBeenCalledTimes(6);
+          expect(fn).toHaveBeenNthCalledWith(1, 0);
+          expect(fn).toHaveBeenNthCalledWith(2, 1);
+          expect(fn).toHaveBeenNthCalledWith(3, 2);
+          expect(fn).toHaveBeenNthCalledWith(4, 3);
+          expect(fn).toHaveBeenNthCalledWith(5, 4);
+          expect(fn).toHaveBeenNthCalledWith(6, 5);
+        }
+      );
+    });
   });
 });
--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -10,3 +10,93 @@ export const round = (num: number, acc: number = 0) => {
   let m = Math.pow(10, acc);
   return Math.round((num + Number.EPSILON) * m) / m;
 };
+
+/**
+ * Returns a promise that resolves after `timeout` milliseconds with the value of `value`.
+ *
+ * @param timeout Milliseconds to wait before resolving the promise.
+ * @param value Value that will be used to resolve the promise (optional).
+ */
+export function wait<T>(timeout: number, value?: T): Promise<T> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(value);
+    }, timeout);
+  });
+}
+
+/**
+ * Much like {@link wait} but can be used in the middle of a promise chain.
+ *
+ * @param timeout Milliseconds to wait before resolving with the given value.
+ * @returns A function that can be passed to `Promise.then`.
+ */
+export function delay<T>(timeout: number): (value: T) => Promise<T> {
+  return (value) => {
+    return wait(timeout, value);
+  };
+}
+
+async function constantRetry<T>(
+  fn: (retries?: number) => Promise<T>,
+  interval: number,
+  maxRetries: number,
+  retry: number
+): Promise<T> {
+  try {
+    return await fn(retry);
+  } catch (e) {
+    if (retry >= maxRetries) {
+      throw e;
+    }
+
+    await wait(interval);
+    return constantRetry(fn, interval, maxRetries, retry + 1);
+  }
+}
+
+/**
+ * RetryOptions controls the behaviour of {@link retry}.
+ */
+type RetryOptions = {
+  type: "constant";
+
+  /**
+   * maxRetries is the number of retries that will be tried before failing.
+   * Use Infinity to continue (practically) forever.
+   */
+  maxRetries?: number;
+
+  /**
+   * interval is the number of milliseconds that will be slept between the retries.
+   */
+  interval?: number;
+};
+
+/**
+ * retry will retry to resolve the promise returned by `fn` as defined by `options`.
+ *
+ * @param fn Function returning a {@link Promise},
+ * will be called with the retry count for each retry
+ * (that is, for the very first try, it will be called with `0`, then with `1`, `2`, and so on so forth).
+ * @param options Controls the retry logic, for all options see the variants of the {@link RetryOptions} type.
+ */
+export async function retry<T>(
+  fn: (retries?: number) => Promise<T>,
+  options: RetryOptions
+): Promise<T> {
+  const p = fn(0);
+
+  if (options.type == "constant") {
+    const interval = options.interval ?? 500;
+    const maxRetries = options.maxRetries ?? 5;
+
+    return p.catch((e) => {
+      if (maxRetries <= 0) throw e;
+
+      return constantRetry(fn, interval, maxRetries, 1);
+    });
+  }
+
+  throw "unsupported retry type";
+}
