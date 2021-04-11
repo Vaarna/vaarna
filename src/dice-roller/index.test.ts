b7ab7ae94215d05e5ecb61e1e8ea4a21d635663b
Maximilian Remming <maximilian@remming.fi>
2021-04-11 21:18:53 +0300

    Implement RegEx based dice rolling for simple cases

--- src/dice-roller/index.test.ts
+++ src/dice-roller/index.test.ts
@@ -1,5 +1,59 @@
+import fc from "fast-check";
 import { roll } from "./index";
 
-test("roll can be called", () => {
-  expect(roll("")).toEqual([]);
+test.each([
+  ["d1", 1, 1],
+  ["d1+1", 2, 2],
+  ["d1-1", 0, 0],
+])("rolling %s results between %s and %s", (v, low, high) => {
+  const ress = roll(v);
+  expect(ress).toHaveLength(1);
+  const res = ress[0];
+  expect(res.total).toBeGreaterThanOrEqual(low);
+  expect(res.total).toBeLessThanOrEqual(high);
+});
+
+test("result of rolling an N-sided dice is between 1 and N", () => {
+  fc.assert(
+    fc.property(fc.integer(1, Number.MAX_SAFE_INTEGER), (n) => {
+      const ress = roll(`d${n}`);
+      expect(ress).toHaveLength(1);
+      const res = ress[0];
+      expect(res.total).toBeGreaterThanOrEqual(1);
+      expect(res.total).toBeLessThanOrEqual(n);
+    })
+  );
+});
+
+test("result of rolling K N-sided dice is between K and K*N", () => {
+  fc.assert(
+    fc.property(
+      fc.integer(1, 2 ** 16),
+      fc.integer(1, Number.MAX_SAFE_INTEGER),
+      (k, n) => {
+        const ress = roll(`${k}d${n}`);
+        expect(ress).toHaveLength(1);
+        const res = ress[0];
+        expect(res.total).toBeGreaterThanOrEqual(k);
+        expect(res.total).toBeLessThanOrEqual(k * n);
+      }
+    )
+  );
+});
+
+test("result of rolling K N-sided dice modified by M is between K+M and K*N+M", () => {
+  fc.assert(
+    fc.property(
+      fc.integer(1, 2 ** 16),
+      fc.integer(1, Number.MAX_SAFE_INTEGER),
+      fc.integer(),
+      (k, n, m) => {
+        const ress = m > 0 ? roll(`${k}d${n}+${m}`) : roll(`${k}d${n}-${-m}`);
+        expect(ress).toHaveLength(1);
+        const res = ress[0];
+        expect(res.total).toBeGreaterThanOrEqual(k + m);
+        expect(res.total).toBeLessThanOrEqual(k * n + m);
+      }
+    )
+  );
 });
--- src/dice-roller/index.ts
+++ src/dice-roller/index.ts
@@ -1,3 +1,43 @@
-export function roll(_v: string): string[] {
-  return [];
+const simpleDiceRollRe = /(?<count>\d+)?d(?<sides>\d+)((?<sign>[+-])(?<mod>\d+))?/i;
+
+type Dice = {
+  count: number;
+  sides: number;
+};
+
+type DiceResult = {
+  result: number;
+} & Dice;
+
+type RollResult = {
+  total: number;
+  dice: DiceResult[];
+}[];
+
+export function roll(v: string): RollResult {
+  const res = simpleDiceRollRe.exec(v)?.groups;
+
+  if (res === undefined) {
+    throw new Error("invalid input");
+  }
+
+  const count = parseInt(res?.count ?? "1", 10);
+  const sides = parseInt(res?.sides ?? "1", 10);
+  const sign = res?.sign ?? "+";
+  const modAmount = parseInt(res?.mod ?? "0", 10);
+
+  const rolls = new Array(count).fill(sides).map((v) => Math.ceil(Math.random() * v));
+  const result = rolls.reduce((prev, cur) => prev + cur, 0);
+  const dice = [{ count, sides, result }];
+
+  let mod = 0;
+  if (sign === "+") {
+    mod += modAmount;
+  } else if (sign === "-") {
+    mod -= modAmount;
+  }
+
+  const total = result + mod;
+
+  return [{ total, dice }];
 }
