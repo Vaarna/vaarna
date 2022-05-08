9081b8f8892bc929a5d36bd1dcded34938931efc
Maximilian Remming <maximilian@remming.fi>
2022-05-09 00:47:09 +0300

    Fix type error with _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,16 @@ import "normalize.css/normalize.css";
 import "../styles/reset.css";
 import "../styles/master.css";
 
-import React, { FC } from "react";
+import React from "react";
 import { AppProps } from "next/app";
 import { wrapper } from "store";
 
-const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
-);
+const WrappedApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const C = Component as any;
+
+  // TODO: find out why the type of Component cannot be used here...
+  return <C {...pageProps} />;
+};
 
 export default wrapper.withRedux(WrappedApp);
