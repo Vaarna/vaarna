f5625bceb86dd6f2cc0dc73e17a3e3d97e2ccfd4
Maximilian Remming <maximilian@remming.fi>
2022-05-08 23:30:59 +0300

    Remove CDK and testcafe

--- test/util.ts
+++
@@ -1,8 +0,0 @@
-import { ClientFunction, Selector } from "testcafe";
-
-export const windowPathname = ClientFunction(() => window.location.pathname);
-export const getPage = (path: string = "/"): string =>
-  (process.env.DOMAIN ?? "http://localhost:3000") + path;
-
-export const setSpaceId = (t: TestController, spaceId: string): TestControllerPromise =>
-  t.typeText(Selector("#spaceId"), spaceId);
