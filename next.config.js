9df6e462d3bfaacb89ad8ea9bb9e137bf967b6b1
Maximilian Remming <maximilian@remming.fi>
2022-05-13 21:46:58 +0300

    Add cyclic dependency checking and fix tsconfig

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
+const CircularDependencyPlugin = require("circular-dependency-plugin");
+
 module.exports = {
   webpack5: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    return {
+      ...config,
+      plugins: [
+        ...config.plugins,
+        new CircularDependencyPlugin({
+          exclude: /node_modules/,
+          include: /pages|src/,
+          failOnError: false,
+          allowAsyncCycles: false,
+          cwd: process.cwd(),
+        }),
+      ],
+    };
+  },
 };
