11a910e56ca28a093820f67ed37e0b7972089805
Maximilian Remming <maximilian@remming.fi>
2022-05-14 21:59:21 +0300

    Remove circular dependency checks and move to swc

--- next.config.js
+++ next.config.js
@@ -1,23 +1,8 @@
-const CircularDependencyPlugin = require("circular-dependency-plugin");
-
-module.exports = {
-  webpack5: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    const circularDependency = dev
-      ? [
-          new CircularDependencyPlugin({
-            exclude: /node_modules/,
-            include: /pages|src/,
-            failOnError: false,
-            allowAsyncCycles: false,
-            cwd: process.cwd(),
-          }),
-        ]
-      : [];
-
-    return {
-      ...config,
-      plugins: [...config.plugins, ...circularDependency],
-    };
-  },
+/**
+ * @type {import('next').NextConfig}
+ */
+const config = {
+  swcMinify: true,
 };
+
+module.exports = config;
