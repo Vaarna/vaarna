614c2eabf3052d4c652e723d35021913b7321878
Maximilian Remming <maximilian@remming.fi>
2022-05-14 20:44:27 +0300

    Enable circular dependency checks only on dev mode

--- next.config.js
+++ next.config.js
@@ -3,19 +3,21 @@ const CircularDependencyPlugin = require("circular-dependency-plugin");
 module.exports = {
   webpack5: true,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Important: return the modified config
+    const circularDependency = dev
+      ? [
+          new CircularDependencyPlugin({
+            exclude: /node_modules/,
+            include: /pages|src/,
+            failOnError: false,
+            allowAsyncCycles: false,
+            cwd: process.cwd(),
+          }),
+        ]
+      : [];
+
     return {
       ...config,
-      plugins: [
-        ...config.plugins,
-        new CircularDependencyPlugin({
-          exclude: /node_modules/,
-          include: /pages|src/,
-          failOnError: false,
-          allowAsyncCycles: false,
-          cwd: process.cwd(),
-        }),
-      ],
+      plugins: [...config.plugins, ...circularDependency],
     };
   },
 };
