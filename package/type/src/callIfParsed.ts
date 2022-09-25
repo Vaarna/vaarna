92f51bf8b35e449f1c717c7ac8d395a04ac09770
Maximilian Remming <maximilian@remming.fi>
2022-09-25 11:54:42 +0300

    Remove unnecessary type constraint

--- package/type/src/callIfParsed.ts
+++ package/type/src/callIfParsed.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const callIfParsed =
-  <T extends unknown>(t: z.ZodType<T>, f: (a: T) => void) =>
+  <T>(t: z.ZodType<T>, f: (a: T) => void) =>
   (v: unknown): void => {
     const parsed = t.safeParse(v);
     if (parsed.success) f(parsed.data);
