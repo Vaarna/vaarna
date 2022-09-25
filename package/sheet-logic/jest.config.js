39f8e974adc4ee986dd055d3f7df3bdc68b403e2
Maximilian Remming <maximilian@remming.fi>
2022-09-25 10:24:00 +0300

    Make sure package.json files are up-to-date

--- package/sheet-logic/jest.config.js
+++ package/sheet-logic/jest.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
   transform: {
     "^.+\\.tsx?$": ["@swc/jest"],
   },
   extensionsToTreatAsEsm: [".ts", ".tsx"],
 };
+
+module.exports = config;
