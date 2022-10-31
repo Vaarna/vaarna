3019a623343cc2dc8bbc6c1637533394b1b51aee
Maximilian Remming <maximilian@remming.fi>
2022-10-31 19:21:21 +0200

    Fix type errors

--- package/logging/src/asAWSLogger.ts
+++ package/logging/src/asAWSLogger.ts
@@ -1,19 +1,19 @@
 import P from "pino";
 
 type AWSLogger = {
-  debug: (_: never) => void;
-  info: (_: never) => void;
-  warn: (_: never) => void;
-  error: (_: never) => void;
+  debug: (...content: never[]) => void;
+  info: (...content: never[]) => void;
+  warn: (...content: never[]) => void;
+  error: (...content: never[]) => void;
 };
 
 type AWSClient = "DynamoDB" | "S3";
 export function asAWSLogger(client: AWSClient, logger: P.Logger): AWSLogger {
   const l = logger.child({ client });
   return {
-    debug: (v) => l.trace(v, client),
-    info: (v) => l.debug(v, client),
-    warn: (v) => l.warn(v, client),
-    error: (v) => l.error(v, client),
+    debug: (...v) => l.trace({}, ...v),
+    info: (...v) => l.debug({}, ...v),
+    warn: (...v) => l.warn({}, ...v),
+    error: (...v) => l.error({}, ...v),
   };
 }
